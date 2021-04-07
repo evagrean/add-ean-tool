a3f37db486add51dace97075bf42803492c60fff
eva <me@example.com>
2021-04-07 14:02:09 +0200

    deleted test code

--- src/upload-file.ts
+++ src/upload-file.ts
@@ -3,6 +3,10 @@ import multer from "multer";
 // import { v4 as uuid4 } from "uuid";
 import csv from "csvtojson";
 
+const app = express();
+
+app.use(express.static("public"));
+
 // allows us to customize the way that multe stores the files
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
@@ -14,21 +18,6 @@ const storage = multer.diskStorage({
   },
 });
 const upload = multer({ storage });
-
-// // test: convert uploaded csv to json
-
-// const csvFilePath = `/uploads/${storage.filename}`;
-// console.log(csvFilePath);
-
-// csv()
-//   .fromFile(csvFilePath)
-//   .then((jsonObj) => {
-//     console.log(jsonObj);
-//   });
-
-const app = express();
-
-app.use(express.static("public"));
 
 app.post("/upload", upload.single("report"), (req, res) => {
   return res.json({ status: "OK" });
